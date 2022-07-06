caa450b9b5c04042e8a6062524e907dd0d1e52a7
Mansi Maurya <mansimaurya0@gmail.com>
2022-07-06 11:36:12 +0530

    Done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
-import { BadRequestError} from '../errors/index.js'
+import { BadRequestError, UnAuthenticatedError } from '../errors/index.js'
 
 const register = async (req, res) => {
   const { name, email, password } = req.body
@@ -26,16 +26,41 @@ const register = async (req, res) => {
     location: user.location,
   })
 }
+const login = async (req, res) => {
+  const { email, password } = req.body
+  if (!email || !password) {
+    throw new BadRequestError('Please provide all values')
+  }
+  const user = await User.findOne({ email }).select('+password')
+  if (!user) {
+    throw new UnAuthenticatedError('Invalid Credentials')
+  }
 
-const login = async (req,res) => {
-    console.log('login user')
-    res.send('login user')
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect) {
+    throw new UnAuthenticatedError('Invalid Credentials')
+  }
+  const token = user.createJWT()
+  user.password = undefined
+  res.status(StatusCodes.OK).json({ user, token, location: user.location })
 }
+const updateUser = async (req, res) => {
+  const { email, name, lastName, location } = req.body
+  if (!email || !name || !lastName || !location) {
+    throw new BadRequestError('Please provide all values')
+  }
+  const user = await User.findOne({ _id: req.user.userId })
 
+  user.email = email
+  user.name = name
+  user.lastName = lastName
+  user.location = location
 
-const updateUser = async  (req,res) => {
-    console.log('update user')
-    res.send('update user')
+  await user.save()
+
+  const token = user.createJWT()
+
+  res.status(StatusCodes.OK).json({ user, token, location: user.location })
 }
 
 export { register, login, updateUser }
